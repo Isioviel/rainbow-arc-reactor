b3a4dc7ef6408c0980a3cafaf87eac3708f78e64
Sarah Townson <sarah.townson@scienceoxford.com>
2019-07-18 10:35:05 +0100

    Create rainbow.py

---
+++ rainbow.py
@@ -0,0 +1,32 @@
+from microbit import *
+import neopixel
+
+np = neopixel.NeoPixel(pin1, 12)
+
+red = (50, 0, 0)
+orange = (50, 25, 0)
+yellow = (40, 50, 0)
+green = (0, 50, 0)
+blue = (0, 0, 50)
+indigo = (50, 0, 25)
+violet = (50, 0, 50)
+white = (50, 50, 50)
+
+colours = [red, red, orange, orange, yellow, yellow,
+           green, green, blue, blue, violet, violet]
+
+off = False
+
+while True:
+    while off is False:
+        for pixel in range(0, len(np)):
+            np[pixel] = colours[pixel]
+        np.show()
+        for x in range(0, 2):
+            colours.append(colours.pop(0))
+        sleep(500)
+        if button_a.was_pressed():
+            off = True
+            np.clear()
+    if button_a.was_pressed():
+            off = False
